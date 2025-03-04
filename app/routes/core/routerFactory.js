85355946ff023601b0040cebbd2f781f06a87ed8
DeC <67894454+xengom@users.noreply.github.com>
2025-03-04 20:05:44 +0900

    router pattern

---
+++ app/routes/core/routerFactory.js
@@ -0,0 +1,18 @@
+class RouterFactory {
+  static createRouter(type) {
+    const routers = {
+      account: require('../accountRoutes'),
+      management: require('../managementRoutes'),
+      record: require('../recordRoutes'),
+      replay: require('../replayRoutes')
+    };
+
+    if (!routers[type]) {
+      throw new Error(`Invalid router type: ${type}`);
+    }
+
+    return new routers[type]();
+  }
+}
+
+module.exports = RouterFactory;
