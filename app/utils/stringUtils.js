e0886d45ff57954532ed51cc542f87e7dea7d4d4
codingpoppy94 <60597797+codingpoppy94@users.noreply.github.com>
2025-03-06 03:48:14 +0900

    refactor: 불필요한 utils 삭제

--- app/utils/stringUtils.js
+++ app/utils/stringUtils.js
@@ -2,25 +2,7 @@
  * utils 공통함수 정리
  */
 
-/**
- * # 태그 검증
- * @param {*} str 
- * @returns 
- */
-const validateTag = (str) => {
-    const pattern = /^[가-힣a-zA-Z0-9]{1,16}#[가-힣a-zA-Z0-9]{1,16}$/;
 
-    if (!str) {
-        throw new Error("태그 문자열이 비어있습니다");
-    }
-
-    if (!pattern.test(str)) {
-        console.log("유효하지 않은 태그:", str);
-        throw new Error("잘못된 형식");
-    }
-
-    return true;
-};
 
 /**
  * 데이터 없을 경우 응답
@@ -39,48 +21,6 @@ const notFoundResponse = () => {
 const notFoundAccount = (name, tag) => {
     return `${name}#${tag} 계정이 게임 기록에 존재하지 않습니다.`;
 }
-
-/**
- * 슬래쉬 문자열 분리
- * @param {*} str 
- * @returns 
- */
-const splitStr = (str) => {
-    if (!str) {
-        console.log("입력된 문자열이 비어있습니다");
-        throw new Error("잘못된 형식");
-    }
-
-    const [sub_name, main_name] = str.split('/');
-
-    if (!sub_name || !main_name) {
-        console.log("잘못된 형식의 문자열:", str);
-        throw new Error("잘못된 형식");
-    }
-
-    return [sub_name, main_name];
-};
-
-/**
- * # 문자열 분리
- * @param {*} str 
- * @returns 
- */
-const splitTag = (str) => {
-    if (!str) {
-        console.log("입력된 태그가 비어있습니다");
-        throw new Error("잘못된 형식");
-    }
-
-    const [name, name_tag] = str.split('#');
-
-    if (!name || !name_tag) {
-        console.log("잘못된 태그 형식:", str);
-        throw new Error("잘못된 형식");
-    }
-
-    return [name, name_tag];
-};
 
 
 /**
@@ -106,44 +46,8 @@ const dictPosition = (position) => {
     }
 }
 
-/**
- * 날짜 분리
- * @param {*} date 
- * @returns 
- */
-const splitDate = (date) => {
-    if (date === undefined) {
-        const now = new Date();
-        const year = now.getFullYear().toString();
-        const month = (now.getMonth() + 1).toString().padStart(2, '0');
-        return [year, month];
-    }
-
-    if (typeof date !== 'string') {
-        console.error('유효하지 않은 입력 타입:', typeof date);
-        throw new Error("날짜 형식 오류");
-    }
-
-    const [year, month] = date.split('-');
-
-    if (!year || !month ||
-      isNaN(year) ||
-      isNaN(month) ||
-      month.length !== 2) {
-        console.error('잘못된 날짜 형식:', date);
-        throw new Error("날짜 형식 오류");
-    }
-
-    return [year, month];
-};
-
-
 module.exports = {
     notFoundResponse,
     notFoundAccount,
-    validateTag,
-    splitStr,
-    splitTag,
     dictPosition,
-    splitDate,
 };
